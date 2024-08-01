081164266310afe1a3b01109568b956b9faf1a60
Xiaoyang-Yu <huitian0115@gmail.com>
2024-08-01 01:38:21 -0400

    master分支测试合并冲突 commit

--- DataStructures/src/com/learning/git/GitTest.java
+++ DataStructures/src/com/learning/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("第2次版本提交");
         System.out.println("第3次版本提交");
         System.out.println("hot-fix 第4次版本提交");
+        System.out.println("master分支测试合并冲突");
+
     }
 }
