6fec94f8349f7bd614da368e866604fa59193396
Xiaoyang-Yu <huitian0115@gmail.com>
2024-08-01 01:12:41 -0400

    First Commit

--- DataStructures/src/com/learning/git/GitTest.java
+++ DataStructures/src/com/learning/git/GitTest.java
@@ -3,7 +3,6 @@ package com.learning.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("第1次版本提交");
-        System.out.println("第2次版本提交");
-        System.out.println("第3次版本提交");
+
     }
 }
